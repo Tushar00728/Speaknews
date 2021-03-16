1e81d0e44e5f427ebe4aa53e43ee3d00820b8ac0
Tushar Mehta <56684401+Tushar00728@users.noreply.github.com>
2021-03-16 21:00:05 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == "__main__":
+    speak("News for today, Let's proceed")
+    url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=b009a5a27cf14d7ba82358324a422202"
+    news = requests.get(url).text
+    news_obj = json.loads(news) #string to python object
+    print(news_obj["articles"])
+    artic = news_obj["articles"]
+    for article in artic:
+        speak(article["title"])
+        if(article== list(artic)[-1]):
+            speak("Thats all")
+            continue
+        speak("Moving on..")
+        
+    speak("Thanks for listening")
